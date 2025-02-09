edeb232cfebc1111ce805595a2a467d738fab44b
dylannoel <dtigomo@varisymo.com>
2025-02-09 21:26:40 +0100

    Gesture animation : Learn whileHover, whileTap, whileFocus

--- components/Gestures.js
+++ components/Gestures.js
@@ -11,27 +11,19 @@ const Gestures = () => {
         gap: "0.8rem",
       }}
     >
-      <MotionConfig
-        transition={{
-          duration: 0.125,
-          ease: "easeInOut",
-        }}
-      >
+      <MotionConfig transition={{ duration: 0.1, ease: "circInOut" }}>
         <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95, rotate: "2.5deg" }}
-          className="example-button"
-        >
-          Click me!
-        </motion.button>
+          whileHover={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9, rotate: 10 }}
+          className="example-button">Click me</motion.button>
+
         <motion.button
-          whileHover={{ scale: 1.15 }}
-          whileTap={{ scale: 0.85, rotate: "-2.5deg" }}
-          style={{ backgroundColor: "red" }}
-          className="example-button"
-        >
-          Click me!
-        </motion.button>
+        style={{ background: "black" }}
+          whileHover={{ scale: 1.2 }}
+          whileFocus={{ scale: 1.2 }}
+          whileTap={{ scale: 0.9, rotate: 10 }}
+          className="example-button">Click me</motion.button>
+
       </MotionConfig>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import BasicsOfMotion from "../components/BasicsOfMotion";
-/* import AnimationControls from "../components/AnimationControls.js";
+/* import BasicsOfMotion from "../components/BasicsOfMotion"; */
+import AnimationControls from "../components/AnimationControls.js";
 import Gestures from "../components/Gestures";
 import ScrollAnimations from "../components/ScrollAnimation";
-import ViewBasedAnimations from "../components/ViewBaseAnimaions"; */
+import ViewBasedAnimations from "../components/ViewBaseAnimaions";
 
 export default function Home() {
   return (
     <div>
-      <BasicsOfMotion />
-      {/* <Gestures /> */}
+      {/* <BasicsOfMotion /> */}
+      <Gestures />
       {/* <AnimationControls /> */}
       {/* <ViewBasedAnimations /> */}
       {/* <ScrollAnimations /> */}
